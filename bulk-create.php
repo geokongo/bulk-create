766257c852ce88003342aafaa77b0ea89bb9ed80
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2023-02-19 21:33:06 +0300

    Setting the activate and deactive hooks to work right

--- bulk-create.php
+++ bulk-create.php
@@ -1,27 +1,26 @@
 <?php
 /**
- * @package Bulk Create
+ * Plugin Name          
+ *
+ * @package           PluginPackage
+ * @author            Geoffrey Okongo
+ * @copyright         2023 (c) Geoffrey Okongo
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Bulk Create
+ * Plugin URI:        https://github.com/geokongo/bulk-create
+ * Description:       This plugin will help you create and publish WordPress pages and post in bulk. You can do this from a list of page titles and URL slugs that you can copy directly into the plugin admin area.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Geoffrey Okongo
+ * Author URI:        https://geokongo.com
+ * Text Domain:       bulk-create
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://wordpress.com/plugins/bulk-create
  */
-/*
-Plugin Name: Bulk Create
-Plugin URI: https://wordpress.org/plugins/bulk-create
-Description: This plugin will help you create and publish WordPress pages and post in bulk. You can do this from a list of page titles and URL slugs that you can copy directly into the plugin area of upload a .csv file. I'm also working on a possibility of saving time by simply linking to you Google Sheets and publishing in just one click.
-Version: 1.0.0
-Author: Geoffrey Okongo
-Author URI: https://geokongo.com
-License: MIT
-Text Domain: bulk-create
-*/
-/*
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
-and associated documentation files (the “Software”), to deal in the Software without restriction,
-including without limitation the rights to use, copy, modify, merge, publish, distribute, 
-sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
-is furnished to do so.
-
-Copyright (c) 2020-2023 Geoffrey Okongo
-*/
 
 //If this file is called directly, abort!
 defined('ABSPATH') or die;
@@ -34,23 +33,24 @@ if(file_exists(dirname(__FILE__) . '/vendor/autoload.php')){
 /**
  * The code that runs during plugin activation
  */
-function activate_bulkcreate(){
+function bulkcreate_activate(){
     Geokongo\BulkCreate\Activate::activate();
 }
 
 /**
  * The code that runs during plugin deactivation
  */
-function deactivate_bulkcreate(){
+function bulkcreate_deactivate(){
     Geokongo\BulkCreate\Deactivate::deactivate();
 }
 
-register_activation_hook(__FILE__, 'activate_bulkcreate');
-register_deactivation_hook(__FILE__, 'deactivate_bulkcreate');
+register_activation_hook(__FILE__, 'bulkcreate_activate');
+register_deactivation_hook(__FILE__, 'bulkcreate_deactivate');
 
 /**
  * Initialize all the core classes of the plugin
  */
-if(class_exists('Geokongo\BulkCreate\Init')){
-    Geokongo\BulkCreate\Init::register_services();
+if(class_exists('Geokongo\BulkCreate\Initialize')){
+    $bulkcreate = new Geokongo\BulkCreate\Initialize();
+    $bulkcreate->start();
 }
--- templates/admin.php
+++ templates/admin.php
@@ -1,1 +1,22 @@
-<?php
+<div class="container">
+<div class="wrap row">
+    <div class="col-sm">
+        <p>Column One</p>
+    </div>
+    <div class="col-sm">
+        <p>Column Two</p>
+    </div>
+    <div class="col-sm">
+        <h1>Bulk Create</h1>
+        <?php settings_errors(); ?>
+
+        <form method="post" action="options.php">
+            <?php
+                settings_fields('wp_bulk_create_group');
+                do_settings_sections('wp_bulk_create');
+                submit_button();
+            ?>
+        </form>
+    </div>
+</div>
+</div>
