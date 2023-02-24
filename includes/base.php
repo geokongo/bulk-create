46b0c7af668a53f4199516e0116e45caed6a2f36
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2023-02-24 13:54:12 +0300

    Updating the base class namespace

--- includes/base.php
+++ includes/base.php
@@ -13,7 +13,7 @@ class Base{
     public function __construct(){
         $this->plugin_path = plugin_dir_path(dirname(__FILE__, 1));
         $this->plugin_url = plugin_dir_url(dirname(__FILE__, 1));
-        $this->plugin = plugin_basename(dirname(__FILE__, 2)) . '/bulk-create.php';
+        $this->plugin = plugin_basename(dirname(__FILE__, 2)) . '/go-bulk-create.php';
     }
 
 
